965ed67d5854c7eda66e2be02063cdcbe7aaae28
Robin H. <robin.holt@rocketmail.com>
2026-07-05 17:07:16 +0100

    Replace window handle in example

--- examples/0_link_example/src/main.cpp
+++ examples/0_link_example/src/main.cpp
@@ -4,13 +4,13 @@ int main()
 {
     std::cout << "Hello from example_0" << std::endl;
     archon::WindowManager windowManager;
-    windowManager.createWindow("Greebles", 1000, 1000);
+    windowManager.createWindow("example_0", 1000, 1000);
 
     // We need threading to handle multiple windows at once but the code is here.
     // We will focus on just rendering to a singular window for the time being.
-    while(!glfwWindowShouldClose(windowManager.getWindow("Greebles").getWindow()))
+    while(!glfwWindowShouldClose(windowManager.getWindow("example_0").getWindow()))
     {
-        glfwSwapBuffers(windowManager.getWindow("Greebles").getWindow());
+        glfwSwapBuffers(windowManager.getWindow("example_0").getWindow());
         glfwPollEvents();
     }
     return 0;
