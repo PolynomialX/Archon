08e4f8321daf2a850bc28b9e8c6c7f101dfdfb5f
Robin H. <robin.holt@rocketmail.com>
2026-07-05 16:46:12 +0100

    Replaces test code in example 0 to show linking

--- examples/0_link_example/src/main.cpp
+++ examples/0_link_example/src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
-
+#include "window_tools/WindowManager.h"
 int main()
 {
-    std::cout << "Hello from example_0\n";
+    std::cout << "Hello from example_0" << std::endl;
+    archon::WindowManager windowManager;
+    windowManager.createWindow("Greebles", 1000, 1000);
 
+    // We need threading to handle multiple windows at once but the code is here.
+    // We will focus on just rendering to a singular window for the time being.
+    while(!glfwWindowShouldClose(windowManager.getWindow("Greebles").getWindow()))
+    {
+        glfwSwapBuffers(windowManager.getWindow("Greebles").getWindow());
+        glfwPollEvents();
+    }
     return 0;
 }
