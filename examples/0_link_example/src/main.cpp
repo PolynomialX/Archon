199988a82b1399504c357eefd4eac01203689f46
Robin H. <robin.holt@rocketmail.com>
2026-07-08 18:24:43 +0100

    Removes comments from main.cpp in example_0

--- examples/0_link_example/src/main.cpp
+++ examples/0_link_example/src/main.cpp
@@ -22,13 +22,6 @@ int main()
     }
     
    archon::RenderOrchestrator::simpleRenderLoop(exampleWindow);
-
-    // We need threading to handle multiple windows at once but the code is here.
-    // We will focus on just rendering to a singular window for the time being.
-    // while(!glfwWindowShouldClose(windowManager.getWindow("example_0").getWindow()))
-    // {
-    //     glfwSwapBuffers(windowManager.getWindow("example_0").getWindow());
-    //     glfwPollEvents();
-    // }
+   
     return 0;
 }
