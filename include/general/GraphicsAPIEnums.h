aca8230590a6eba4c320c052f1e898a00ab48cbc
Robin H. <robin.holt@rocketmail.com>
2026-07-21 19:22:26 +0100

    Adds GraphicsAPI header - whether this will be used is TBD

---
+++ include/general/GraphicsAPIEnums.h
@@ -0,0 +1,12 @@
+#ifndef ARCHON_GRAPHICS_API_ENUMS_H
+#define ARCHON_GRAPHICS_API_ENUMS_H_
+namespace archon
+{
+enum class GraphicsAPI
+{
+    OPEN_GL,
+    VULKAN,
+    DIRECTX
+};
+} // archon
+#endif // ARCHON_GRAPHICS_API_ENUMS_H_
