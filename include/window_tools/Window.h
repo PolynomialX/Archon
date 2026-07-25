eb680ab66b99d3d6cf6bf0372a73b3722eff1ff0
Robin H. <robin.holt@rocketmail.com>
2026-07-25 11:45:19 +0100

    Adds Window header back and conan archive

---
+++ include/window_tools/Window.h
@@ -0,0 +1,33 @@
+// Rename to Window after
+#ifndef ARCHON_WINDOW_TOOLS_WINDOW_H_
+#define ARCHON_WINDOW_TOOLS_WINDOW_H_
+#include <string>
+namespace archon
+{
+namespace window_tools
+{
+
+/**
+ * @class Window
+ * @brief Interface for Window objects
+ */
+class Window
+{
+public:
+    virtual int getWidth() const = 0;
+    virtual int getHeight() const = 0;
+    virtual const std::string& getTitle() const = 0;
+    virtual void setHeight(int height_) = 0;
+    virtual void setWidth(int width_) = 0;
+    virtual void setTitle(const std::string& title_) = 0;
+    virtual void makeActive() = 0;
+    virtual bool shouldClose() const = 0;
+    virtual void swapBuffers() = 0;
+private:
+};
+
+} // window_tools
+} // archon
+
+
+#endif // ARCHON_WINDOW_TOOLS_WINDOW_H_
